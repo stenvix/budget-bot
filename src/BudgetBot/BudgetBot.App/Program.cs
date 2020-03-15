a4f88b0d569eaba9d1e06bce59f1302bc97623f3
Valentyn Stepanov <falken.ua@gmail.com>
2020-03-16 00:13:53 +0200

    Add serilog

---
+++ src/BudgetBot/BudgetBot.App/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Reflection.Metadata.Ecma335;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Hosting.Internal;
+using Serilog;
+
+namespace BudgetBot.App
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await CreateHostBuilder(args).RunConsoleAsync();
+        }
+
+        private static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            var hostBuilder = Host.CreateDefaultBuilder(args);
+            hostBuilder.UseSerilog((context, configuration) =>
+            {
+                configuration.ReadFrom.Configuration(context.Configuration);
+            });
+
+            return hostBuilder;
+        }
+    }
+}
